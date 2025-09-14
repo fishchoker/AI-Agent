59e80569610094fb60d1918606d187c522807dad
fishchoker <1191556590@qq.com>
2025-09-14 19:35:23 +0800

    脱敏

---
+++ ai-agent-station-study-main/ai-agent-station-study-domain/src/main/java/cn/bugstack/ai/domain/agent/service/armory/AiClientApiNode.java
@@ -0,0 +1,104 @@
+package cn.bugstack.ai.domain.agent.service.armory;
+
+import cn.bugstack.ai.domain.agent.model.entity.ArmoryCommandEntity;
+import cn.bugstack.ai.domain.agent.model.valobj.enums.AiAgentEnumVO;
+import cn.bugstack.ai.domain.agent.model.valobj.AiClientApiVO;
+import cn.bugstack.ai.domain.agent.service.armory.factory.DefaultArmoryStrategyFactory;
+import cn.bugstack.wrench.design.framework.tree.StrategyHandler;
+import com.alibaba.fastjson.JSON;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+/**
+ * OpenAI API配置节点
+ *
+ * @author xiaofuge bugstack.cn @小傅哥
+ * 2025/7/1 07:09
+ */
+@Slf4j
+@Service
+public class AiClientApiNode extends AbstractArmorySupport {
+
+    @Resource
+    private AiClientToolMcpNode aiClientToolMcpNode;
+    
+    @Autowired
+    @Qualifier("pgVectorJdbcTemplate")
+    private JdbcTemplate jdbcTemplate;
+
+
+    @Override
+    protected String doApply(ArmoryCommandEntity requestParameter, DefaultArmoryStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        log.info("Ai Agent 构建节点，API 接口请求{}", JSON.toJSONString(requestParameter));
+
+        List<AiClientApiVO> aiClientApiList = dynamicContext.getValue(dataName());
+
+        if (aiClientApiList == null || aiClientApiList.isEmpty()) {
+            log.warn("没有需要被初始化的 ai client api");
+            return router(requestParameter, dynamicContext);
+        }
+
+        for (AiClientApiVO aiClientApiVO : aiClientApiList) {
+            // 构建 OpenAiApi
+            OpenAiApi openAiApi = OpenAiApi.builder()
+                    .baseUrl(aiClientApiVO.getBaseUrl())
+                    .apiKey(aiClientApiVO.getApiKey())
+                    .completionsPath(aiClientApiVO.getCompletionsPath())
+                    .embeddingsPath(aiClientApiVO.getEmbeddingsPath())
+                    .build();
+			
+			  // 打印 baseUrl 和 completionsPath
+			  log.info("组装结果 - baseUrl: {}, completionsPath: {}",
+			  aiClientApiVO.getBaseUrl(), aiClientApiVO.getCompletionsPath());
+			 
+
+            // 注册 OpenAiApi Bean 对象
+            registerBean(beanName(aiClientApiVO.getApiId()), OpenAiApi.class, openAiApi);
+            log.info("Ai Agent 构建节点，构建向量模型{}");
+            // 2. 构建 Embedding Model
+            OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
+
+            // 3. 构建 PgVectorStore
+			/*
+			 * PgVectorStore vectorStore = PgVectorStore.builder(jdbcTemplate,
+			 * embeddingModel) .vectorTableName("vector_store_" + apiVO.getApiId()) // 动态命名
+			 * .build();
+			 */
+            PgVectorStore vectorStore = PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                    .vectorTableName("vector_store_openai") // 固定表名
+                    .build();
+
+            // 4. 注册 Bean
+            registerBean("vectorStore", PgVectorStore.class, vectorStore);
+        }
+
+        return router(requestParameter, dynamicContext);
+    }
+
+    @Override
+    public StrategyHandler<ArmoryCommandEntity, DefaultArmoryStrategyFactory.DynamicContext, String> get(ArmoryCommandEntity armoryCommandEntity, DefaultArmoryStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        return aiClientToolMcpNode;
+    }
+
+    @Override
+    protected String beanName(String beanId) {
+        return AiAgentEnumVO.AI_CLIENT_API.getBeanName(beanId);
+    }
+
+    @Override
+    protected String dataName() {
+        return AiAgentEnumVO.AI_CLIENT_API.getDataName();
+    }
+
+}
